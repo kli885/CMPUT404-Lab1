ec37ac3e3009c8bdbeab46ac134c6349ee0c1584
Kevin <kli7@ualberta.ca>
2021-01-11 18:20:25 -0700

    script.py

--- script.py
+++ script.py
@@ -1,4 +1,11 @@
 import requests
-print(requests.__version__)
 
+#Gets the current version of requests package
+version = requests.__version__
+
+#Gets the google Homepage
 google = requests.get("https://www.google.com")
+
+#Gets the prints the script.py file uploaded on Github
+github = requests.get("https://raw.github.com/kli885/CMPUT404-Lab1/master/script.py")
+print(github.text)
